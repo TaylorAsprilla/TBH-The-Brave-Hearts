e3a0719aba3d640846920beceda87d7f448449f9
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2024-03-20 14:48:16 -0500

    La sesión se cierra en 2 horas de inactividad

--- src/environments/configuration/config.ts
+++ src/environments/configuration/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   // HH: MM: SS: MS
-  INACTIVE_TIMEOUT_MS: 1 * 10 * 60 * 1000, // 1 hora en milisegundos
+  INACTIVE_TIMEOUT_MS: 2 * 60 * 60 * 1000, // 1 hora en milisegundos
   TIMER_MODEL: 5 * 60 * 1000, // 5 minutos en milisegundos
 };
